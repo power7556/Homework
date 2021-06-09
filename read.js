911a7177893c8e76c0051d7e4fa0e5e89e503b01
xogns1904 <xogns1904@naver.com>
2021-06-09 23:22:42 +0900

    1차 r기능

---
+++ read.js
@@ -0,0 +1,27 @@
+var friend = 'tom';
+var friend = 'julie';
+var friend = 'paul';
+var friend = 'lisa';
+var friend = 'jack';
+
+console.log("Tom을 검색합니다.")
+if(friend = 'tom')
+{
+    console.log("Tom", 19, "남");
+}
+else if(friend = 'julie')
+{
+    console.log("Julie", 17, "여");
+}
+else if(friend = 'paul')
+{
+    console.log("Paul", 24, "남");
+}
+else if(friend = 'lisa')
+{
+    console.log("Lisa", 21, "여");
+}
+else
+{
+    console.log("Jack", 20, "남");
+}
